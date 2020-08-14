cf872c5ce51111e9fa9fd8854da0208fa085d109
tang <923233700@qq.com>
2020-08-14 11:40:43 +0800

    准备上传

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import x from './x.js'
-console.log(x)
-console.log('hi')
+import x from "./x.js";
+import png from "./assets/1.png";
+
+const div = document.getElementById("app");
+div.innerHTML = `
+  <img src="${png}">
+`;
+
+const button = document.createElement("button");
+button.innerText = "懒加载";
+button.onclick = () => {
+  const promise = import("./lazy");
+  promise.then(
+    (module) => {
+      const fn = module.default;
+      fn();
+    },
+    () => {}
+  );
+};
+
+div.appendChild(button);
